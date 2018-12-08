d26193f39dc5547fd872f3ccc166c32d07cf577e
zengaorong@qq.com <7monthdleo>
2018-12-08 17:00:39 +0800

    单点登录-用户权限配置-测试添加用户

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,13 +1,9 @@
-#coding=utf-8
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField,TextAreaField,HiddenField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from wtforms import ValidationError
 from ..models import User
 
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
-photos = UploadSet('photos', IMAGES)
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
@@ -17,19 +13,11 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Log In')
 
 
-class Addmhname(FlaskForm):
-    mhname = StringField('mhname', validators=[
-        DataRequired(), Length(1, 64)])
-    submit = SubmitField('mhname')
-
 class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Length(1, 64),
                                              Email()])
     username = StringField('Username', validators=[
-        DataRequired(), Length(1, 64),
-        Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-               'Usernames must have only letters, numbers, dots or '
-               'underscores')])
+        DataRequired(), Length(1, 64),])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
@@ -43,23 +31,35 @@ class RegistrationForm(FlaskForm):
         if User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in use.')
 
-class UploadForm(FlaskForm):
-    photo = FileField(validators=[
-        FileAllowed(photos, u'只能上传图片！'),
-        FileRequired(u'文件未选择！')])
-    submit = SubmitField(u'上传')
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm new password',
+                              validators=[DataRequired()])
+    submit = SubmitField('Update Password')
 
 
-class manhuafrom(FlaskForm):
-    photo = FileField(validators=[
-        FileAllowed(photos, u'只能上传图片！'),
-        FileRequired(u'文件未选择！')])
-    submit = SubmitField(u'上传')
+class PasswordResetRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
+                                             Email()])
+    submit = SubmitField('Reset Password')
 
-class fromtest(FlaskForm):
-    id = StringField('id', validators=[DataRequired(), Length(1, 36)])
-    mhname = StringField('mhname', validators=[DataRequired(), Length(1, 64)])
-    hidden_pic_url = HiddenField("pic_url",default="")
-    # picurl = StringField('picurl', validators=[DataRequired(), Length(1, 128)])
-    submit = SubmitField('提交')
 
+class PasswordResetForm(FlaskForm):
+    password = PasswordField('New Password', validators=[
+        DataRequired(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,28 +1,23 @@
-#coding=utf-8
-from flask import render_template, redirect, request, url_for, flash,jsonify,send_from_directory
+from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, logout_user, login_required, \
     current_user
 from . import auth
 from .. import db
-from ..models import User,Manhua,Chapter
+from ..models import User
 from ..email import send_email
-from .forms import LoginForm, RegistrationForm, Addmhname,UploadForm,photos,fromtest
-from sqlalchemy import and_
-import os
-import re
-import sys
+from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
+    PasswordResetRequestForm, PasswordResetForm, ChangeEmailForm
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-dataname = "leodb"
+
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated \
-            and not current_user.confirmed \
-            and request.endpoint \
-            and request.blueprint != 'auth' \
-            and request.endpoint != 'static':
-        return redirect(url_for('auth.unconfirmed'))
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint \
+                and request.blueprint != 'auth' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
@@ -95,231 +90,80 @@ def resend_confirmation():
     return redirect(url_for('main.index'))
 
 
-@auth.route('/redirect', methods=['GET', 'POST'])
-def leo_redirect():
-    form = RegistrationForm()
+@auth.route('/change-password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    form = ChangePasswordForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data,
-                    username=form.username.data,
-                    password=form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-
-
-
-
-@auth.route('/manhuaname', methods=['GET', 'POST'])
-def get_manhuaname():
-    form = Addmhname()
-    if form.validate_on_submit():
-        manhua  = Manhua(mhname=form.mhname.data)
-        db.session.add(manhua)
-        db.session.commit()
-    return render_template('auth/addnewmh.html', form=form)
-    #return redirect(url_for('auth.leo_redirect'))
-
-
-@auth.route('/leomahua')
-def leomahua():
-    list = Manhua.query.order_by(Manhua.mhname)
-    return render_template('auth/showmh.html', data_list=list)
-
-@auth.route('/leopic')
-def leopic():
-    url_list = []
-    return render_template('auth/showpic.html', url_list=url_list)
-
-
-# @auth.route('/add',methods=['GET', 'POST'])
-# def add_numbers():
-#     chapter_id = request.args.get('chapter_id', '', type=str)
-#
-#     chapter = Chapter.query.filter_by(id = chapter_id).first()
-#     chapter_strs = chapter.data
-#     chapter_list = chapter_strs.split('\n')
-#     chapter_list.pop()
-#     return jsonify(result = chapter_list)
-
-
-@auth.route('/getindexdata',methods=['GET', 'POST'])
-def getindexdata():
-    mhlist = Manhua.query.all()
-    return jsonify(result = mhlist)
-
-
-
-
-@auth.route('/addpic/<chapter_id>')
-def addpic(chapter_id):
-    return render_template('auth/showpics.html',chapter_id=chapter_id)
-
-
-@auth.route('/mhindex',methods=['GET', 'POST'])
-def mhindex():
-    mhlist = Manhua.query.all()
-    return render_template('auth/mhindex.html',mhlist = mhlist)
-
-# @auth.route('/index',methods=['GET', 'POST'])
-# def index():
-#     mhlist = Manhua.query.all()
-#     return render_template('auth/index.html',mhlist = mhlist)
-
-
-# @auth.route('/mhchapter/<mh_id>',methods=['GET', 'POST'])
-# def mhchapter(mh_id):
-#     chapters = Chapter.query.filter_by(mhname_id = mh_id).all()
-#     return render_template('auth/mhchapter.html',chapters = chapters)
-
-
-
-def getDate():
-    mhlist = Manhua.query.all()
-    for key in  mhlist:
-        #print key.mhname
-        chapters = Chapter.query.filter_by(mhname_id = key.id).all()
-        for chapter in chapters:
-            print chapter.chapter_name
-
-
-
-    mhname = '火影忍者'
-    mh = Manhua.query.filter_by(mhname=mhname).first()
-    chapters = Chapter.query.filter_by(mhname_id = mh.id).all()
-    chapter_strs = chapters[0].data
-    chapter_list = chapter_strs.split('\n')
-    chapter_list.pop()
-    #return chapter_list
-
-
-
-@auth.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(os.getcwd() + '/app/static/upload',filename)
-
-
-
-@auth.route('/', methods=['GET', 'POST'])
-def upload_file():
-    form = UploadForm()
-    if form.validate_on_submit():
-        filename = photos.save(form.photo.data)
-        file_url = photos.url(filename)
-    else:
-        file_url = None
-    return render_template('auth/upload.html', form=form, file_url=file_url)
-
-
-
-def test3(chaptername,check_str,mhname):
-    str_type = check_str.split('.')[-1]
-    str_name = chaptername
-
-    pattern = re.compile(u'(.+)\.+(.+)')
-    match = pattern.match(check_str)
-    str_id =  match.group(1)
-
-    str_nums = str_id.replace(chaptername,'')
-    if str_nums.find('num')!=-1:
-        str_nums = check_str.split('.')[0].replace('num','')
-
-    return [str_id,str_type,mhname,str_name,str_nums,mhname + '/' + str_name + '/' + check_str]
-
-
-
-# http://flask-uploads.readthedocs.io/en/latest/
-@auth.route('/uploadImage', methods=['POST'])
-def flask_upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            #logger.debug('No file part')
-            return jsonify({'code': -1, 'filename': '', 'msg': 'No file part'})
-        file = request.files['file']
-        # if user does not select file, browser also submit a empty part without filename
-        if file.filename == '':
-            #logger.debug('No selected file')
-            return jsonify({'code': -1, 'filename': '', 'msg': 'No selected file'})
+        if current_user.verify_password(form.old_password.data):
+            current_user.password = form.password.data
+            db.session.add(current_user)
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('main.index'))
         else:
-            # try:
-                filename = photos.save(file)
-                #logger.debug('%s url is %s' % (filename, uploaded_photos.url(filename)))
-                return jsonify({'code': 0, 'filename': filename, 'msg': photos.url(filename)})
-            # except Exception as e:
-            #     #logger.debug('upload file exception: %s' % e)
-            #     return jsonify({'code': -1, 'filename': '', 'msg': 'Error occurred'})
-    else:
-        return jsonify({'code': -1, 'filename': '', 'msg': 'Method not allowed'})
+            flash('Invalid password.')
+    return render_template("auth/change_password.html", form=form)
 
 
-@auth.route('/mhupload',methods=['GET', 'POST'])
-def mhupload():
-    return render_template('auth/uploadtest.html')
+@auth.route('/reset', methods=['GET', 'POST'])
+def password_reset_request():
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetRequestForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            token = user.generate_reset_token()
+            send_email(user.email, 'Reset Your Password',
+                       'auth/email/reset_password',
+                       user=user, token=token)
+        flash('An email with instructions to reset your password has been '
+              'sent to you.')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_password.html', form=form)
 
 
-# 修改漫画名称
-@auth.route('/showlist', methods=['GET', 'POST'])
-def get_showlist():
-    mhlist = Manhua.query.all()
-    return render_template('auth/from.html',mhlist = mhlist)
+@auth.route('/reset/<token>', methods=['GET', 'POST'])
+def password_reset(token):
+    if not current_user.is_anonymous:
+        return redirect(url_for('main.index'))
+    form = PasswordResetForm()
+    if form.validate_on_submit():
+        if User.reset_password(token, form.password.data):
+            db.session.commit()
+            flash('Your password has been updated.')
+            return redirect(url_for('auth.login'))
+        else:
+            return redirect(url_for('main.index'))
+    return render_template('auth/reset_password.html', form=form)
 
 
-@auth.route('/from/<mh_id>', methods=['GET', 'POST'])
-def mhfrom(mh_id):
-    mhdata = Manhua.query.filter_by(id=mh_id).first()
-    leofrom = fromtest()
-    if leofrom.validate_on_submit():
-        mhdata.mhname = leofrom.mhname.data
-        mhdata.pic_url = leofrom.hidden_pic_url.data
+@auth.route('/change_email', methods=['GET', 'POST'])
+@login_required
+def change_email_request():
+    form = ChangeEmailForm()
+    if form.validate_on_submit():
+        if current_user.verify_password(form.password.data):
+            new_email = form.email.data
+            token = current_user.generate_email_change_token(new_email)
+            send_email(new_email, 'Confirm your email address',
+                       'auth/email/change_email',
+                       user=current_user, token=token)
+            flash('An email with instructions to confirm your new email '
+                  'address has been sent to you.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Invalid email or password.')
+    return render_template("auth/change_email.html", form=form)
 
-        db.session.add(mhdata)
+
+@auth.route('/change_email/<token>')
+@login_required
+def change_email(token):
+    if current_user.change_email(token):
         db.session.commit()
-        flash('the profile has been updated')
-        return redirect(url_for('auth.get_showlist'))
-    leofrom.mhname.data = mhdata.mhname
-    leofrom.id.data = mhdata.id
-    leofrom.hidden_pic_url.data = mhdata.pic_url
-    file_url = mhdata.pic_url
-    return render_template('auth/mhfrom.html',mhdata = mhdata,leofrom=leofrom, file_url = file_url)
-
-# @auth.route('/addchapter', methods=['GET', 'POST'])
-# def addchapter():
-#     mhlist = Manhua.query.all()
-#     return render_template('auth/uploadtest.html',mhlist = mhlist)
-
-# @auth.route('/readvideo', methods=['GET', 'POST'])
-# def readvideo():
-#     return render_template('auth/b.html')
-
-
-# @auth.route('/manhualist', methods=['GET', 'POST'])
-# def manhualist():
-#     page = request.args.get('page', 1, type=int)
-#     pagination = Manhua.query.order_by(Manhua.id.desc()).paginate(
-#         page, per_page=2,
-#         error_out=False)
-#     posts = pagination.items
-#     return render_template('auth/loglist.html',posts=posts,pagination=pagination)
-
-
-# @auth.route('/wordtest',methods=['GET', 'POST'])
-# def wordtest():
-#     # chapter_id = request.args.get('chapter_id', '', type=str)
-#     #
-#     # chapter = Chapter.query.filter_by(id = chapter_id).first()
-#     # chapter_strs = chapter.data
-#     # chapter_list = chapter_strs.split('\n')
-#     # chapter_list.pop()
-#
-#     words = [request.args.get('mhname', '', type=str)]
-#     # words = ['钢']
-#     for w in words:
-#         print w
-#     rule = and_(*[Manhua.mhname.like('%' + w + '%') for w in words])
-#     print rule
-#     manhua = Manhua.query.filter(rule)
-#     chapter_list =  ['测试','测试1']
-#     for key in manhua:
-#         chapter_list.append(key.mhname)
-#
-#     return jsonify(result = chapter_list)
+        flash('Your email address has been updated.')
+    else:
+        flash('Invalid request.')
+    return redirect(url_for('main.index'))
